9e334923764870f1d146885fbfbb8f68570d6a79
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-08-07 11:12:25 +0530

    make frontend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
-import { Alert } from './components/Alert';
+// import { Alert } from './components/Alert';
 import Signup from './components/Signup';
 import Login from './components/Login';
 
@@ -18,7 +18,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message="This is amazing React course" />
+          {/* <Alert message="This is amazing React course" /> */}
           <div className="container">
             <Switch>
               <Route exact path="/">
