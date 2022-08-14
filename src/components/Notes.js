be98e429fb6147b2fee40e7a17b6f41ebe7f0b12
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-08-14 18:30:42 +0530

    Update Notes.js

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,7 @@ import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const { notes, editNote, getNotes } = context;
+    const { notes, editNote, getNotes, checK_loginOr_not } = context;
 
     // without reload change data
     useEffect(() => {
@@ -32,69 +32,76 @@ const Notes = () => {
 
     return (
         <>
-            <AddNote />
-            <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <form className="my-3">
-                                <div className="mb-3">
-                                    <label htmlFor="title" className="form-label">Title</label>
-                                    <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} minLength={5} required/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="description" className="form-label">Description</label>
-                                    <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="tag" className="form-label">Tag</label>
-                                    <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
-                                </div>
-
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button disabled={note.etitle.length<5 || note.edescription.length<5} onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+            { checK_loginOr_not ? (
+                <>
+                <AddNote />
+                <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                </button>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form className="my-3">
+                                    <div className="mb-3">
+                                        <label htmlFor="title" className="form-label">Title</label>
+                                        <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} minLength={5} required/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="description" className="form-label">Description</label>
+                                        <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="tag" className="form-label">Tag</label>
+                                        <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
+                                    </div>
+    
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button disabled={note.etitle.length<5 || note.edescription.length<5} onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="row my-3">
-                <h2>You Notes</h2>
-                <div className="container mx-2">
-                    {notes.length === 0 && 'No notes to display'}
-                </div>
-                {/* {notes.map((note) => (
-                    <>
-                        <div className="col-md-3">
-                            <div className="card my-3">
-                                <div className="card-body">
-                                    <div className="d-flex align-items-center">
-                                        <h5 className="card-title">{note.title}</h5>
-                                        <i className="far fa-trash-alt mx-2" ></i>
-                                        <i className="far fa-edit mx-2" ></i>
+                <div className="row my-3">
+                    <h2>You Notes</h2>
+                    <div className="container mx-2">
+                        {notes.length === 0 && 'No notes to display'}
+                    </div>
+                    {/* {notes.map((note) => (
+                        <>
+                            <div className="col-md-3">
+                                <div className="card my-3">
+                                    <div className="card-body">
+                                        <div className="d-flex align-items-center">
+                                            <h5 className="card-title">{note.title}</h5>
+                                            <i className="far fa-trash-alt mx-2" ></i>
+                                            <i className="far fa-edit mx-2" ></i>
+                                        </div>
+                                        <p className="card-text">{note.description}</p>
+    
                                     </div>
-                                    <p className="card-text">{note.description}</p>
-
                                 </div>
                             </div>
-                        </div>
-                    </>
-                ))} */}
-
-                {notes.map((note) => {
-                    return <Noteitem key={note._id} updateNote={updateNote} note={note} />
-                })}
-
-            </div>
+                        </>
+                    ))} */}
+    
+                    {notes.map((note) => {
+                        return <Noteitem key={note._id} updateNote={updateNote} note={note} />
+                    })}
+    
+                </div>
+                </>
+            ) : (
+                <div className="my-3">login kar abe</div>
+            )}
+            
         </>
     )
 }
