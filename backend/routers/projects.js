cf70f1c3f446b39268b97b67c210c3914e240ff9
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-08-16 10:43:56 +0530

    edit project name

--- backend/routers/projects.js
+++ backend/routers/projects.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
-const Note = require('../models/Project');
+const Project = require('../models/Project');
 const { body, validationResult } = require('express-validator');
 
-// ROUTE 1: Get All the Notes using: GET "/api/projects/getuser". Login required
-router.get('/fetchallnotes', fetchuser, async (req, res) => {
+// ROUTE 1: Get All the Projects using: GET "/api/projects/fetchallprojects". Login required
+router.get('/fetchallprojects', fetchuser, async (req, res) => {
     try {
-        const notes = await Note.find({ user: req.user.id });
-        res.json(notes)
+        const pro = await Project.find({ user: req.user.id });
+        res.json(pro)
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
 })
 
-// ROUTE 2: Add a new Note using: POST "/api/projects/addnote". Login required
-router.post('/addnote', fetchuser, [
+// ROUTE 2: Add a new Project using: POST "/api/projects/addproject". Login required
+router.post('/addproject', fetchuser, [
     body('Projectname', 'Enter a valid Projectname').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
     body('progess', 'Enter a valid Progess').isNumeric({ min: 0, max: 100 })], async (req, res) => {
@@ -28,12 +28,12 @@ router.post('/addnote', fetchuser, [
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
             }
-            const note = new Note({
+            const pro = new Project({
                 Projectname, description, tag, progess, githublink, user: req.user.id
             })
-            const savedNote = await note.save()
+            const savedpro = await pro.save()
 
-            res.json(savedNote)
+            res.json(savedpro)
 
         } catch (error) {
             console.error(error.message);
@@ -41,47 +41,47 @@ router.post('/addnote', fetchuser, [
         }
     })
 
-// ROUTE 3: Update an existing Note using: PUT "/api/projects/updatenote". Login required
-router.put('/updatenote/:id', fetchuser, async (req, res) => {
+// ROUTE 3: Update an existing Project using: PUT "/api/projects/updateProject". Login required
+router.put('/updateproject/:id', fetchuser, async (req, res) => {
     const { Projectname, description, tag, progess, githublink } = req.body;
     try {
-        // Create a newNote object
-        const newNote = {};
-        if (Projectname) { newNote.Projectname = Projectname };
-        if (description) { newNote.description = description };
-        if (tag) { newNote.tag = tag };
-        if (progess) { newNote.progess = progess };
-        if (githublink) { newNote.githublink = githublink };
+        // Create a newProject object
+        const newProject = {};
+        if (Projectname) { newProject.Projectname = Projectname };
+        if (description) { newProject.description = description };
+        if (tag) { newProject.tag = tag };
+        if (progess) { newProject.progess = progess };
+        if (githublink) { newProject.githublink = githublink };
         
-        // Find the note to be updated and update it
-        let note = await Note.findById(req.params.id);
-        if (!note) { return res.status(404).send("Not Found") }
+        // Find the Project to be updated and update it
+        let pro = await Project.findById(req.params.id);
+        if (!pro) { return res.status(404).send("Not Found") }
 
-        if (note.user.toString() !== req.user.id) {
+        if (pro.user.toString() !== req.user.id) {
             return res.status(401).send("Not Allowed");
         }
-        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-        res.json({ note });
+        pro = await Project.findByIdAndUpdate(req.params.id, { $set: newProject }, { new: true })
+        res.json({ pro });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
 })
 
-// ROUTE 4: Delete an existing Note using: DELETE "/api/projects/deletenote". Login required
-router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+// ROUTE 4: Delete an existing Project using: DELETE "/api/projects/deleteProject". Login required
+router.delete('/deleteproject/:id', fetchuser, async (req, res) => {
     try {
-        // Find the note to be delete and delete it
-        let note = await Note.findById(req.params.id);
-        if (!note) { return res.status(404).send("Not Found") }
+        // Find the Project to be delete and delete it
+        let pro = await Project.findById(req.params.id);
+        if (!pro) { return res.status(404).send("Not Found") }
 
-        // Allow deletion only if user owns this Note
-        if (note.user.toString() !== req.user.id) {
+        // Allow deletion only if user owns this Project
+        if (pro.user.toString() !== req.user.id) {
             return res.status(401).send("Not Allowed");
         }
 
-        note = await Note.findByIdAndDelete(req.params.id)
-        res.json({ "Success": "Note has been deleted", note: note });
+        pro = await Project.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Project has been deleted", project_info: pro });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
