fb1ca60623e031f5036b526334d6478a789d7cd2
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-08-18 19:03:59 +0530

    add ai admin

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -32,6 +32,7 @@ router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', 'Enter a valid email').isEmail(),
   body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+
 ], async (req, res) => {
   let success = false;
   // If there are errors, return Bad request and the errors
@@ -53,6 +54,7 @@ router.post('/createuser', [
       name: req.body.name,
       password: secPass,
       email: req.body.email,
+      image: req.body.image,
     });
     const data = {
       user: {
@@ -111,12 +113,52 @@ router.post('/login', [
     console.error(error.message);
     res.status(500).send("Internal Server Error");
   }
+});
 
+// that part is remaining
+// ROUTE 3: Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/loginbyface', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  let success = false;
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      success = false
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      success = false
+      return res.status(400).json({ success, error: "Please try to login with correct credentials" });
+    }
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    success = true;
+    res.json({ success, authtoken })
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 
-// ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
+// ROUTE 4: Get loggedin User Details using: POST "/api/auth/getuser". Login required
 router.post('/getuser', fetchuser,  async (req, res) => {
 
   try {
