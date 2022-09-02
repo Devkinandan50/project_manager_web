0c82e6282856503c1f23fbe8e3115fc51b3db26d
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-09-02 11:37:19 +0530

    add signup img

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = 5000
 // })
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit:'50mb'}))
 app.use('/api/auth', require('./routers/auth'))
 app.use('/api/projects', require('./routers/projects'))
 
