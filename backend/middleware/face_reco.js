324641ec7b2d2393b9281ce20dd127e9cc8e46cd
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-09-25 00:14:03 +0530

    SOME CHANGES

--- backend/middleware/face_reco.js
+++ backend/middleware/face_reco.js
@@ -1,6 +1,31 @@
 const faceapi = require('face-api.js');
+const path = require('path');
+const fetch = require('node-fetch');
+
+// const tf = require('@tensorflow/tfjs-node');
+// const Canvas = require('canvas');
+// const Image = require('canvas');
+// const ImageData = require('canvas');
+
+// faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+faceapi.env.monkeyPatch({ fetch: fetch });
+
+const MODELS_URL = path.join(__dirname, '/models');
 
 const face_re = (storeImg, log_Img, name) => {
+    Promise.all([
+        async function loadAllmodel() {
+            await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODELS_URL);
+            await faceapi.nets.faceLandmark68Net.loadFromDisk(MODELS_URL);
+            await faceapi.nets.faceRecognitionNet.loadFromDisk(MODELS_URL);
+            await faceapi.nets.faceExpressionNet.loadFromUri(MODELS_URL);
+            await faceapi.nets.tinyFaceDetector.loadFromUri(MODELS_URL);
+          }
+      ])
+        .then(console.log("done model"))
+        .catch((e) => console.log(e));
+
+
     if(storeImg == log_Img){
         return true;
     }
@@ -11,3 +36,6 @@ const face_re = (storeImg, log_Img, name) => {
 
 
 module.exports = face_re;
+
+
+
