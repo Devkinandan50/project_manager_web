5a443b049af842d871c1994ad61d2a36d14013be
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-11-22 23:45:54 +0530

    add selection board

--- src/components/charts/DashBoard.js
+++ src/components/charts/DashBoard.js
@@ -1,12 +1,39 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Select, MenuItem, FormHelperText, FormControl, InputLabel } from '@material-ui/core';
+import ProjectContext from "../../context/pro_jects/projectContext";
 
 
 const DashBoard = () => {
+    const [selectedProjectName, setselectedProjectName] = useState("")
+
+    const Procontext = useContext(ProjectContext);
+    const { getProjects_forDashboard, all_dashboardprojects } = Procontext;
+    
+
+    useEffect(() => {
+        getProjects_forDashboard();
+    }, [])
+
+    function displayemployeelist(item) {
+        return <MenuItem value={item.Projectname} onClick={(e) => setselectedProjectName(e.target.outerText)}>{item.Projectname} </MenuItem>
+    }
 
     return (
         <div>
-            <h1>barchart</h1>
-        </div>
+            <FormControl variant="outlined" style={{ width: 200, margin: 10 }}>
+                <Select label="Project" name='project' defaultValue={'All'}>
+                    {
+                        all_dashboardprojects.map(displayemployeelist)
+                    }
+                </Select>
+            </FormControl>
+
+            {selectedProjectName ? (
+                <h1> {selectedProjectName}</h1>
+            ) : (
+                <></>
+            )}
+        </div >
     )
 };
 export default DashBoard;
