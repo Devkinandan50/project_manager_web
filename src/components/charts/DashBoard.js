b944fd107c40f86bb469d45aa74a2ce7e29339dd
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2023-01-04 15:13:57 +0530

    Update DashBoard.js

--- src/components/charts/DashBoard.js
+++ src/components/charts/DashBoard.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Select, MenuItem, FormHelperText, FormControl, InputLabel } from '@material-ui/core';
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles, useTheme } from "@material-ui/core/styles";
 import ProjectContext from "../../context/pro_jects/projectContext";
 import './Dashboard.css'
 import Widgets from './widget/Widgets';
@@ -12,23 +14,26 @@ const DashBoard = () => {
     const [selectedProjectName, setselectedProjectName] = useState("All")
 
     const [all_dashboardprojects, setall_dashboardprojects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const theme = useTheme();
 
 
     // Get all Projects for dashboard select with all  
-  const getProjects_forDashboard = async () => {
-    // API Call 
-      const response = await fetch(`${host}/api/projects/fetchallprojects`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          "auth-token": authtoken
-        }
-      });
-      const json = await response.json()
-      // console.log(json)
-      json.unshift({Projectname: "All"});
-      setall_dashboardprojects(json);
-  }
+    const getProjects_forDashboard = async () => {
+        // API Call 
+        const response = await fetch(`${host}/api/projects/fetchallprojects`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": authtoken
+            }
+        });
+        const json = await response.json()
+        // console.log(json)
+        json.unshift({ Projectname: "All" });
+        setall_dashboardprojects(json);
+        setLoading(false);
+    }
 
     useEffect(() => {
         getProjects_forDashboard();
@@ -52,38 +57,58 @@ const DashBoard = () => {
                     </FormControl>
                 </div>
             </div>
-            
 
-            
-            <div className="container" style={{ boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.1)', borderRadius: '1rem', padding: '2rem 1rem 1rem 1rem' }} >
-            {/* <div className="container" style={{ padding: '2rem 1rem 1rem 1rem' }} > */}
 
-                <div style={{marginBottom: '1rem'}}>
-                    <Widgets />
+
+
+            {loading ? (
+                <div
+                    style={{
+                        position: "absolute",
+                        left: "45%",
+                        top: "50%",
+                        marginLeft: "-50px",
+                        marginTop: "-50px",
+                        padding: theme.spacing(1, 9),
+                    }}
+                >
+                    <CircularProgress size={100} />
+                    <h5 style={{ fontSize: 20 }}> Loading..... </h5>
                 </div>
+            ) : (
+                <>
+                    <div className="container" style={{ boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.1)', borderRadius: '1rem', padding: '2rem 1rem 1rem 1rem' }} >
+                        {/* <div className="container" style={{ padding: '2rem 1rem 1rem 1rem' }} > */}
 
-                <div class="row mt-4">
-                    <div class="col-lg-7 mb-lg-0 mb-4">
-                        <div class="dev" style={{ height: '25rem', boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.2)', borderRadius: '1rem' }}>
-                            <div style={{ background: 'linear-gradient(82.59deg, #ff647c 0%, #0084f4 100%)', height: '0.5rem', borderRadius: '1rem' }}></div>
-                            <div style={{ marginLeft: '2rem' }}>
-                                <h4> Project Items status Summary </h4>
+                        <div style={{ marginBottom: '1rem' }}>
+                            <Widgets />
+                        </div>
+
+                        <div class="row mt-4">
+                            <div class="col-lg-7 mb-lg-0 mb-4">
+                                <div class="dev" style={{ height: '25rem', boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.2)', borderRadius: '1rem' }}>
+                                    <div style={{ background: 'linear-gradient(82.59deg, #ff647c 0%, #0084f4 100%)', height: '0.5rem', borderRadius: '1rem' }}></div>
+                                    <div style={{ marginLeft: '2rem' }}>
+                                        <h4> Project Items status Summary </h4>
+                                    </div>
+                                    {/* <PieGraph board={props.selectedboard}  /> */}
+                                </div>
                             </div>
-                            {/* <PieGraph board={props.selectedboard}  /> */}
+                            <div class="col-lg-5">
+                                <div class="dev" style={{ height: '25rem', boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.2)', borderRadius: '1rem' }}>
+                                    <div style={{ background: 'linear-gradient(69.83deg, #0084f4 0%, #00c48c 100%)', height: '0.5rem', borderRadius: '1rem' }}></div>
+                                    <div style={{ marginLeft: '2rem' }}>
+                                        <h4> Project Items status Summary </h4>
+                                    </div>
+
+                                    {/* <Tableofdata board={props.selectedboard} /> */}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-5">
-                        <div class="dev" style={{ height: '25rem', boxShadow: '2px 10px 20px rgba(0, 0, 0, 0.2)', borderRadius: '1rem' }}>
-                            <div style={{ background: 'linear-gradient(69.83deg, #0084f4 0%, #00c48c 100%)', height: '0.5rem', borderRadius: '1rem' }}></div>
-                            <div style={{ marginLeft: '2rem' }}>
-                                <h4> Project Items status Summary </h4>
-                            </div>
+                </>
+            )}
 
-                            {/* <Tableofdata board={props.selectedboard} /> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
         </>
     );
 }
