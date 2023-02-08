1b25172269fe682bb008b38a42012871d0519f00
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2023-02-08 19:45:33 +0530

    add features

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import ProjectContext from "../context/pro_jects/projectContext";
 import Dashboard from './charts/DashBoard';
 import '../style/homepage.css';
 import success from "../img/mainimg.png";
+import successs from "../img/storage-service.jpg";
 
 
 export const Home = () => {
@@ -38,6 +39,57 @@ export const Home = () => {
                         </div>
 
                     </section>
+
+
+                    <section id="service" class="services pt-0">
+                        <div class="container" data-aos="fade-up">
+
+                            <div class="section-header">
+                                <span>Our Services</span>
+                                <h2>Our Services</h2>
+
+                            </div>
+
+                            <div class="row gy-4">
+
+                                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src={successs} alt="" class="img-fluid"/>
+                                        </div>
+                                        <h3><a href="service-details.html" class="stretched-link">Storage</a></h3>
+                                        <p>Cumque eos in qui numquam. Aut aspernatur perferendis sed atque quia voluptas quisquam repellendus temporibus itaqueofficiis odit</p>
+                                    </div>
+                                </div>
+                                {/* <!-- End Card Item --> */}
+
+                                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src={successs} alt="" class="img-fluid"/>
+                                        </div>
+                                        <h3><a href="service-details.html" class="stretched-link">Logistics</a></h3>
+                                        <p>Asperiores provident dolor accusamus pariatur dolore nam id audantium ut et iure incidunt molestiae dolor ipsam ducimus occaecati nisi</p>
+                                    </div>
+                                </div>
+                                {/* <!-- End Card Item --> */}
+
+                                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src={successs} alt="" class="img-fluid"/>
+                                        </div>
+                                        <h3><a href="service-details.html" class="stretched-link">Cargo</a></h3>
+                                        <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p>
+                                    </div>
+                                </div>
+                                {/* <!-- End Card Item --> */}
+
+                            </div>
+
+                        </div>
+                    </section>
+                    {/* <!-- End Services Section --> */}
                 </>
             )}
         </div>
